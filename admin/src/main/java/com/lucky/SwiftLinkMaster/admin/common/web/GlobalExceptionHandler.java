b3524dee11de02a6d67681758edc50f4a13c82e8
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-14 20:55:03 +0800

    feature：开发简链通全局异常拦截器

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,82 @@
+package com.lucky.SwiftLinkMaster.admin.common.web;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/14 17:57
+ * @Description: 全局异常处理器
+ * @Position: com.lucky.SwiftLinkMaster.admin.common.web
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import com.lucky.SwiftLinkMaster.admin.common.convention.errorcode.BaseErrorCode;
+import com.lucky.SwiftLinkMaster.admin.common.convention.exception.AbstractException;
+import com.lucky.SwiftLinkMaster.admin.common.convention.result.Result;
+import com.lucky.SwiftLinkMaster.admin.common.convention.result.Results;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+@Component
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 拦截参数验证异常
+     * @SneakyThrows 是 Lombok 提供的一个注解，它的作用是
+     * 自动抛出受检异常（Checked Exception），而不需要手动编写 try-catch 或 throws 关键字。
+     */
+    @SneakyThrows
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String exceptionStr = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+        log.error("[{}] {} [ex] {}", request.getMethod(), getUrl(request), exceptionStr);
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr);
+    }
+
+    /**
+     * 拦截应用内抛出的异常
+     * @ExceptionHandler 用于 在 Spring Boot 中全局捕获特定类型及其子类的异常，然后执行指定的异常处理逻辑。
+     */
+    @ExceptionHandler(value = {AbstractException.class})
+    public Result abstractException(HttpServletRequest request, AbstractException ex) {
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString(), ex.getCause());
+            return Results.failure(ex);
+        }
+        log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString());
+        return Results.failure(ex);
+    }
+
+    /**
+     * 拦截未捕获异常，Throwable 顶级异常
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        return Results.failure();
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
