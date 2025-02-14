1b3bb1f40df0f51d85a14f9bcdb94cdff8749e43
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-14 20:55:37 +0800

    feature：开发用户自定义异常码

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,31 @@
+package com.lucky.SwiftLinkMaster.admin.common.enums;
+
+import com.lucky.SwiftLinkMaster.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/13 21:17
+ * @Description: TODO
+ * @Position: com.lucky.SwiftLinkMaster.admin.common.enums
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+public enum UserErrorCodeEnum implements IErrorCode {
+    USER_NULL("B000200","用户记录不存在"),
+    USER_EXIST("B000201","用户记录已存在");
+    private final String code;
+    private final String message;
+    UserErrorCodeEnum(String code,String message){
+        this.code = code;
+        this.message = message;
+    }
+    @Override
+    public String code(){
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
