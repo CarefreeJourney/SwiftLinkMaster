2aa9866868156b98b0c9f40aa60eb60ffe16bb45
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-21 16:39:54 +0800

    feature：通过分布式锁完善用户注册功能，防止恶意请求短时间内恶意注册同一个用户名，导致全部请求打到数据库

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,13 @@
+package com.lucky.SwiftLinkMaster.admin.common.constant;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/21 16:21
+ * @Description: 简链通后管 Redis 缓存常量类
+ * @Position: com.lucky.SwiftLinkMaster.admin.common.constant
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "SwiftLinkMaster:lock_user_register:";
+}
--- admin/src/main/java/com/lucky/SwiftLinkMaster/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/service/impl/UserServiceImpl.java
@@ -13,8 +13,12 @@ import com.lucky.SwiftLinkMaster.admin.dto.resp.UserRespDTO;
 import com.lucky.SwiftLinkMaster.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.lucky.SwiftLinkMaster.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 
 /**
  * @Author: lcl
@@ -29,6 +33,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -57,13 +62,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())){
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
         }
-        // 2. 用户名不存在，则将新用户存入数据库中
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        // 3. 判断插入是否成功
-        if (inserted<1){
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY+requestParam.getUsername());
+        try {
+            if (lock.tryLock()){ // 尝试获取锁，成功则进行数据库和缓存的插入
+                // 2. 用户名不存在，则将新用户存入数据库中
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                // 3. 判断插入是否成功
+                if (inserted<1){
+                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+            }else {
+                // 否则抛出用户名已存在的失败信息
+                throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+            }
+        }finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
-
 }
