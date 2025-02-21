c9b77b24602bec7befdb30183577f19455a46089
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-21 16:08:32 +0800

    feature：开发简链通后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,34 @@
+package com.lucky.SwiftLinkMaster.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/15 11:22
+ * @Description: TODO
+ * @Position: com.lucky.SwiftLinkMaster.admin.config
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0 );
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
