ee13c9fd2e18ce3476ab509928d93666a2bc6415
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-23 17:52:31 +0800

    feature：简链通短链接分组功能 抽象Do 数据库持久层基础属性 -- 创建时间+更新时间+软删除标识

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/common/database/BaseDO.java
@@ -0,0 +1,36 @@
+package com.lucky.SwiftLinkMaster.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/23 17:46
+ * @Description: 数据库持久层基础属性 -- 创建时间+更新时间+软删除标识
+ * @Position: com.lucky.SwiftLinkMaster.admin.common.database
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+@Data
+public class BaseDO {
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
