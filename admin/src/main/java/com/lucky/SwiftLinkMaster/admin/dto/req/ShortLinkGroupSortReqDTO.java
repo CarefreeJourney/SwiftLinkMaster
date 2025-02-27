4f851668452afcee3caa2be3ccbd23769aae95da
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-27 17:49:27 +0800

    refactor：后管短链接删除和排序功能实现

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/dto/req/ShortLinkGroupSortReqDTO.java
@@ -0,0 +1,17 @@
+package com.lucky.SwiftLinkMaster.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/24 18:27
+ * @Description: TODO
+ * @Position: com.lucky.SwiftLinkMaster.admin.dto.req
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+@Data
+public class ShortLinkGroupSortReqDTO {
+    private String gid;
+    private Integer sortOrder;
+}
