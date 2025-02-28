bec59192d41af35a942d1cee38c8eacdd6e7a8ad
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-28 10:50:52 +0800

    feature:新增短链接描述字段

--- project/src/test/java/com/lucky/SwiftLinkMaster/project/test/ShortLinkTableShardingTest.java
+++ project/src/test/java/com/lucky/SwiftLinkMaster/project/test/ShortLinkTableShardingTest.java
@@ -25,6 +25,7 @@ public class ShortLinkTableShardingTest {
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  `favicon` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '网站图标',\n" +
             "  PRIMARY KEY (`id`),\n" +
             "  UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
             ") ENGINE=InnoDB AUTO_INCREMENT=1895027695156023299 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
