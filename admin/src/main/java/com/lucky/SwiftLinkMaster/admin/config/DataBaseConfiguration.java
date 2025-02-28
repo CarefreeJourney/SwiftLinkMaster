bc13498318c9ba563a212d67a21d2359bbe7b9cb
ChuliLin <11589546+lin-chuli@user.noreply.gitee.com>
2025-02-28 10:48:16 +0800

    feature：mysql数据库分页插件拦截器的配置

---
+++ admin/src/main/java/com/lucky/SwiftLinkMaster/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,29 @@
+package com.lucky.SwiftLinkMaster.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: lcl
+ * @CreateTime: 2025/2/28 10:27
+ * @Description: TODO
+ * @Position: com.lucky.SwiftLinkMaster.project.config
+ * @CurrentVersion: 1.0
+ * @VersionHistory: 1.0
+ */
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
